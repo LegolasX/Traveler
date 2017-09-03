bdfcbc43dc00a75ed1043d4a2496ec8884d0ac46
LegolasX <Legolas.Invoker@gmail.com>
2017-09-03 10:13:58 +0800

    temp

---
+++ Travellers/Travellers/UIColor + extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor + extension.swift
+//  Travellers
+//
+//  Created by Legolas Invoker on 2017/9/2.
+//  Copyright © 2017年 Chang.Jing. All rights reserved.
+//
+
+import Foundation
---
+++ Travellers/Travellers/cell3.swift
@@ -0,0 +1,24 @@
+//
+//  cell3.swift
+//  Travellers
+//
+//  Created by Legolas Invoker on 2017/9/2.
+//  Copyright © 2017年 Chang.Jing. All rights reserved.
+//
+
+import UIKit
+
+class cell3: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
